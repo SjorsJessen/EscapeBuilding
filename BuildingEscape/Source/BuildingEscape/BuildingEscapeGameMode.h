f616bb75a6234488b400a578a62abb88452110a3
Sjors Jessen <sjors.jessen@gmail.com>
2018-11-27 21:42:33 +0100

    Added GameMode & a related blueprint to create custom DefaultPawn functionality

---
+++ BuildingEscape/Source/BuildingEscape/BuildingEscapeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE_API ABuildingEscapeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
